ac0ced4f96af89e4f3200cbf26dc4d8beb9b4e39
congdang102 <95157465+congdang102@users.noreply.github.com>
2021-12-01 14:51:08 +0700

    update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -5,11 +5,36 @@ showSlides();
 function showSlides() {
   var i;
   var slides = document.querySelectorAll('.js-mySlides');
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
+  for (i = 0; i < slides.length; i++) { 
+    slides[i].style.display = "none"; //
   }
-  slideIndex++;
+  slideIndex= slideIndex +1;
   if (slideIndex > slides.length) {slideIndex = 1}    
   slides[slideIndex-1].style.display = "block";  
   setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+//slide main post
+var slideIndexPost = 1;
+showSlidesPost(slideIndexPost);
+
+function plusSlides(n) {
+  showSlidesPost(slideIndexPost += n);
+}
+
+function currentSlide(n) {
+  showSlidesPost(slideIndexPost = n);
+}
+
+function showSlidesPost(n) {
+  var j;
+  var slidesPost = document.getElementsByClassName("mySlidesPost");
+
+  if (n > slidesPost.length) {slideIndexPost = 1}    
+  if (n < 1) {slideIndexPost = slidesPost.length}
+  for (i = 0; i < slidesPost.length; i++) {
+      slidesPost[i].style.display = "none";  
+  }
+ 
+  slidesPost[slideIndexPost-1].style.display = "block";  
+ 
 }
