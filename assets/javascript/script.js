9ecc94dd352a8c59b2d69b3f4ea7a84f58a8d4ce
NianD16 <88141204+NianD16@users.noreply.github.com>
2021-12-01 15:17:45 +0700

    update

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,38 +3,35 @@ var slideIndex = 0;
 showSlides();
 
 function showSlides() {
-  var i;
-  var slides = document.querySelectorAll('.js-mySlides');
-  for (i = 0; i < slides.length; i++) { 
-    slides[i].style.display = "none"; //
-  }
-  slideIndex= slideIndex +1;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  slides[slideIndex-1].style.display = "block";  
-  setTimeout(showSlides, 5000); // Change image every 5 seconds
+    var i;
+    var slides = document.querySelectorAll('.js-mySlides');
+    for (i = 0; i < slides.length; i++) { 
+        slides[i].style.display = "none"; //
+    }
+    slideIndex= slideIndex +1;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    slides[slideIndex-1].style.display = "block";  
+    setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
 //slide main post
 var slideIndexPost = 1;
 showSlidesPost(slideIndexPost);
 
 function plusSlides(n) {
-  showSlidesPost(slideIndexPost += n);
+    showSlidesPost(slideIndexPost += n);
 }
 
 function currentSlide(n) {
-  showSlidesPost(slideIndexPost = n);
+    showSlidesPost(slideIndexPost = n);
 }
 
 function showSlidesPost(n) {
-  var j;
-  var slidesPost = document.getElementsByClassName("mySlidesPost");
-
-  if (n > slidesPost.length) {slideIndexPost = 1}    
-  if (n < 1) {slideIndexPost = slidesPost.length}
-  for (i = 0; i < slidesPost.length; i++) {
-      slidesPost[i].style.display = "none";  
-  }
- 
-  slidesPost[slideIndexPost-1].style.display = "block";  
- 
+    var j;
+    var slidesPost = document.querySelectorAll('.js-mySlidesPost');
+    if (n > slidesPost.length) {slideIndexPost = 1}    
+    if (n < 1) {slideIndexPost = slidesPost.length}
+    for (j = 0; j < slidesPost.length; j++) {
+        slidesPost[j].style.display = "none";  
+    }
+    slidesPost[slideIndexPost-1].style.display = "block";
 }
